e4445429892330c5b4f6b7bdd722f9c589f70ee6
김규진 <rbrb0014@gmail.com>
2019-12-27 01:05:24 +0900

    crtl+k+f

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    console.log("I did it!");
-    const hasClass = title.classList.contains(CLICKED_CLASS);
-    title.classList.toggle(CLICKED_CLASS)
+  console.log("I did it!");
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-    title.addEventListener("click", handleClick);
+  title.addEventListener("click", handleClick);
 }
 init();
